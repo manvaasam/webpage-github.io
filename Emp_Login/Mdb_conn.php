4a83dbe6192d1a889738d4c7a5ddcc682ae7fea3
Kumar Lakshmanan <klakshmanan48@gmail.com>
2021-09-27 18:36:11 +0530

    User to Customer Chat completed

--- Emp_Login/Mdb_conn.php
+++ Emp_Login/Mdb_conn.php
@@ -1,13 +1,14 @@
 <?php
 
-// $hostname= "localhost";
-// $db_name="manvaasa_login";
-// $unmae= "manvaasa_login";
-// $password = "Iron_Man@#10";
+
 $hostname= "localhost";
-$db_name="manvaasam";
-$unmae= "root";
-$password = "";
+$db_name="manvaasa_login";
+$unmae= "manvaasa_login";
+$password = "Iron_Man@#10";
+// $hostname= "localhost";
+// $db_name="manvaasam";
+// $unmae= "root";
+// $password = "";
 
 
 $conn = mysqli_connect($hostname , $unmae, $password, $db_name);
