86e5b4e973a0651f1ee6974b4f3ec1594990e733
Abiram-manvaasam <abhiram.manvaasam@gmail.com>
2021-12-02 19:11:17 +0530

    update the changes

--- gallery.php
+++ gallery.php
@@ -6,6 +6,13 @@
    <meta name="viewport" content="width=device-width, initial-scale=1">
    <link rel="shortcut icon" href="./image/fav_icon.png" type="image/png">
    <title>Gallery</title>
+   <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MNFWDS3');</script>
+<!-- End Google Tag Manager -->
    <link rel="stylesheet" type="text/css" href="./assets/css/style.001.css">
    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" />
    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
@@ -121,6 +128,10 @@
       </div>
    </div>
    <!--  End of footer Tab  -->
+   <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MNFWDS3"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 </body>
 
 </html>
