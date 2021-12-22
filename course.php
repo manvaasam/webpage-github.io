02ffe0fbd8f868cd82a9dda69c014fb415c56d21
manvaasam <75216215+manvaasam@users.noreply.github.com>
2021-12-22 20:59:50 +0530

    Update course.php

--- course.php
+++ course.php
@@ -23,6 +23,15 @@
     <script>
         new WOW().init();
     </script>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-157973494-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-157973494-1');
+</script>
 </head>
 
 <body>
